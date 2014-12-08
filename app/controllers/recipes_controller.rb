f492ae223c4ce0dbb534a6c45cb6ba841f5267fd
Benoit <benoit@hopsandfork.com>
2014-12-08 13:33:30 +0100

    Update recipe with placeholder render

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,17 @@
 class RecipesController < ApplicationController
 	def index
+		render :text => "index"
 	end
 
 	def show
+		render :text => "show"
 	end
 
 	def new
+		render :text => "new"
 	end
 
 	def create
+		render :text => "create"
 	end	
 end
